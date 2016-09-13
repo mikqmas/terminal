d77f18ff7e46af979239924bc1afc8484d72c076
mikqmas <samkim86@gmail.com>
2016-09-13 16:12:01 -0700

    basic functions

--- js/app.jsx
+++ js/app.jsx
@@ -5,48 +5,71 @@ import React from 'react';
 export default class App extends React.Component {
   constructor(){
     super();
-    this.state = {isEditMode: true, questions: {}};
+    this.state = {isEditMode: true, questions: [], answers: [], truefalse: []};
     this.add_question = this.add_question.bind(this);
     this.change_mode = this.change_mode.bind(this);
+    this.change_question = this.change_question.bind(this);
+    this.change_answer = this.change_answer.bind(this);
+    this.change_question_type = this.change_question_type.bind(this);
   }
 
   add_question(){
-    const nowID = Date.now()
-    this.setState({questions: this.state.questions[nowID] = {id: nowID, q: "", a:"", truefalse: true}
+    let questionsArr = this.state.questions.concat([""]);
+    let answersArr = this.state.answers.concat([""]);
+    let truefalseArr = this.state.truefalse.concat([true]);
+    this.setState({questions: questionsArr, answers: answersArr, truefalse: truefalseArr});
   }
 
   change_mode(){
     this.setState({isEditMode: !this.state.isEditMode});
   }
 
-  change_input(id, val, qora){
-    if(qora === "question"){
-      this.setState(this.state.questions[id][:q] = val);
-    }else {
-      this.setState(this.state.questions[id][:a] = val);
-    }
+  change_question(e){
+    let copyQuestions = this.state.questions
+    copyQuestions[e.target.id] = e.target.value
+    this.setState({questions: copyQuestions});
+  }
+  change_answer(e) {
+    let copyAnswers = this.state.answers
+    copyAnswers[parseInt(e.target.id)] = e.target.value
+    this.setState({answers: copyAnswers});
+  }
+  change_question_type(e) {
+    debugger;
+    let i = parseInt(e.target.id);
+    let copyTrueFalseArr = this.state.truefalse
+    copyTrueFalseArr[i] = e.target.value === "true" ? true : false;
+    let copyAnswers = this.state.answers
+    copyAnswers[i] = ""
+    this.setState({truefalse: copyTrueFalseArr, answers: copyAnswers});
   }
 
   //dropdown truefalse is true, shortanswer is false.
-  change_question_type(id, val) {
-    this.setState({a: "", this.state.problems[id][:truefalse] = (val === 'true'));
-  }
+  // change_question_type(id, val) {
+  //   this.setState({a: "", this.state.problems[id][:truefalse] = (val === 'true'));
+  // }
 
   render() {
     // const editQuestions = this.state.questions.map((question) => {
     //   return <Question key={question.key} onChange={this.handleFieldChange}/>;
     // })
+    let displayQuestions = [];
+    for(let i = 0; i < this.state.questions.length; i++){
+      displayQuestions.push(<Question key={i} id={i} q={this.state.questions[i].q}
+        a={this.state.answers[i]} truefalse={this.state.truefalse[i]} changeAnswer={this.change_answer}
+        changeQuestion={this.change_question} changeType={this.change_question_type}/>)
+    }
     const takeQuiz = <Quiz q={this.state.questions}/>
-    debugger;
     return(
       <div>
         <button onClick={this.change_mode}>{this.state.isEditMode === true ? "Take Test" : "Edit Test"}</button>
         <div id="questions">
-          <Question key=""
-          {this.state.isEditMode ? this.state.questions : takeQuiz}
+          {displayQuestions}
         </div>
         <button onClick={this.add_question} >add question</button>
       </div>
     )
   }
 }
+
+// {this.state.isEditMode ? this.state.questions : takeQuiz}
--- js/question.jsx
+++ js/question.jsx
@@ -3,37 +3,19 @@ import React from 'react';
 export default class Question extends React.Component {
   constructor(props){
     super(props);
-    this.state = {truefalse: true, q: "", a: ""}
-    this.change_question_type = this.change_question_type.bind(this);
-    this.change_input = this.change_input.bind(this);
-  }
-
-  change_input(e){
-    if(e.target.name === "question"){
-      this.setState({q: e.target.value});
-    }else {
-      this.setState({a: e.target.value});
-    }
-  }
-
-  //dropdown truefalse is true, shortanswer is false.
-  change_question_type(e) {
-    this.setState({a: "", truefalse: (e.target.value === 'true')});
   }
 
   render() {
-    debugger;
     let answer;
-    if(this.state.truefalse){
-      answer = (<form onChange={this.change_input}>True<input type="radio" name="truefalse" value="true"/>False<input type="radio" name="truefalse" value="false"/></form>);
+    if(this.props.truefalse){
+      answer = (<form onChange={this.props.changeAnswer}>True<input id={this.props.id} type="radio" name="truefalse" value="true"/>False<input type="radio" id={this.props.id} name="truefalse" value="false"/></form>);
     }else {
-      answer = <input type="text" name="answer" value={this.state.a} onInput={this.change_input} placeholder="answer" />;
+      answer = <input type="text" id={this.props.id} name="answer" value={this.props.a} onChange={this.props.changeAnswer} placeholder="answer" />;
     }
-
     return(
       <div>
-        <input type="text" name="question" value={this.state.q} onInput={this.change_input} placeholder="question" />
-        <select name="question_type" onChange={this.change_question_type}>
+        <input type="text" name="question" id={this.props.id} value={this.props.q} onChange={this.props.changeQuestion} placeholder="question" />
+        <select id={this.props.id} name="question_type" onChange={this.props.changeType}>
           <option value="true">True/False</option>
           <option value="false">Short Answer</option>
         </select>
